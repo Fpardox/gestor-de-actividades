bb77474b70858566714271f4c66184e3daefd249
Francisco Pardo <franciscopardo00@gmail.com>
2025-08-05 14:54:05 -0500

    Añade todas las funcionalidades y mejoras visuales

--- backend/__tests__/api.test.js
+++ backend/__tests__/api.test.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const request = require('supertest');
-const app = require('../server'); // Importamos nuestra app
+const app = require('../server');
 const mongoose = require('mongoose');
 
-// Desconectarse de la base de datos después de todas las pruebas
+// Esta función se ejecuta después de que TODAS las pruebas en este archivo terminan
 afterAll(async () => {
     await mongoose.connection.close();
 });
@@ -11,8 +13,8 @@ describe('API de Tareas', () => {
 
   it('debería obtener una lista de tareas de la ruta /api/tasks', async () => {
     const res = await request(app).get('/api/tasks');
-    expect(res.statusCode).toEqual(200); // Esperamos una respuesta exitosa
-    expect(res.body).toBeInstanceOf(Array); // Esperamos que la respuesta sea un array
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toBeInstanceOf(Array);
   });
 
 });
